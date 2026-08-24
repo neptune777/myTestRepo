392c80b8835b7e4cf1ec27a517f76c3fcfd33440
Matti Kinnunen <matti.k.kinnunen@outlook.com>
2026-08-24 08:47:36 +0300

    Changes for testing Mongoose

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
+const Note = require('./models/note')
+//const cors = require('cors')
 const app = express()
 app.use(express.json())
-app.use(cors())
+//app.use(cors())
 app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
@@ -13,6 +15,7 @@ const requestLogger = (request, response, next) => {
     next()
 }
 app.use(requestLogger)
+
 
 let notes = [
     {
@@ -39,11 +42,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    const object = { name: 'Kauko Vainio', number: 111222 }
-    console.log(typeof object)
-    const json = JSON.stringify(object)
-    console.log(typeof json)
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 const generateId = () => {
@@ -57,30 +58,23 @@ app.post('/api/notes', (request, response) => {
     const body = request.body
 
     if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
+        return response.status(400).json({ error: 'content missing' })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 app.delete('/api/notes/:id', (request, response) => {
     const id = request.params.id
@@ -94,7 +88,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
